16b2b80b73c8881d50ad12ac8f7b4160f97b08e4
David Osumi-Sutherland <dosumis@gmail.com>
2018-08-27 18:16:25 +0100

    Update add_constraints_and_redundant_labels.py
    Fixed rdfs:label reference for 'expression pattern'

--- src/uk/ac/ebi/vfb/neo4j/neo2neo/add_constraints_and_redundant_labels.py
+++ src/uk/ac/ebi/vfb/neo4j/neo2neo/add_constraints_and_redundant_labels.py
@@ -57,7 +57,7 @@ label_types = {
    'Anatomy': 'material anatomical entity',
    'Cell': 'cell',
    'Glial_cell': 'glial cell',
-   'Expression_pattern': 'expression_pattern',
+   'Expression_pattern': 'expression pattern',
    'Ganglion': 'ganglion',
    'Cholinergic': 'cholinergic neuron',
    'Glutamatergic': 'glutamatergic neuron',
