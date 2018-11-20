223888d9636ee3ed6a151a04ce82feadb83297f2
dosumis <dosumis@gmail.com>
2018-11-20 21:48:20 +0000

    Removed none type from pub list.

--- src/uk/ac/ebi/vfb/neo4j/flybase2neo/import_pub_data.py
+++ src/uk/ac/ebi/vfb/neo4j/flybase2neo/import_pub_data.py
@@ -3,6 +3,7 @@ from .pub_tools import pubMover
 from .fb_tools import dict_cursor, get_fb_conn
 from ..neo4j_tools import neo4j_connect, results_2_dict_list
 import re
+import warnings
 
 """Populate pub data.  Should be run as a final step, once all content added."""
 
@@ -18,15 +19,19 @@ nc = neo4j_connect(base_uri, usr, pwd)
 pm = pubMover(base_uri, usr, pwd)
 
 # Pull all pub FBrfs from graph
-statements = ['MATCH (pub) RETURN DISTINCT pub.short_form']
+statements = ['MATCH (pub) RETURN DISTINCT pub.FlyBase'] # Needs to be shifted to short_form - coord with KB.
 results = nc.commit_list(statements)
 if results:
     dc = results_2_dict_list(results)
-    pub_list = [d['pub.short_form'] for d in dc]
+    pub_list = [d['pub.FlyBase'] for d in dc if d['pub.FlyBase']]
+    if pub_list:
+        pm.move(pub_list)
+    else:
+        warnings.warn("No pubs found in %s" % base_uri)
 else:
-    pub_list = []
+    warnings.warn("No pubs found in %s" % base_uri)
 
-pm.move(pub_list)
+
 
 
 # Ways to extend:  
