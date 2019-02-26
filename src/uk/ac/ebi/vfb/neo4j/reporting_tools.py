dff7adfc9ef38b845c972038217d8eacae518c58
dosumis <dosumis@gmail.com>
2019-02-26 11:57:44 +0000

    More reports.

--- src/uk/ac/ebi/vfb/neo4j/reporting_tools.py
+++ src/uk/ac/ebi/vfb/neo4j/reporting_tools.py
@@ -5,47 +5,80 @@ import pandas as pd
 import numpy as np
 
 
-def gen_report(server, query, report_name, column_order = None):
+def gen_report(server, query, report_name, column_order=None):
     """Generates a pandas dataframe with
     the results of a cypher query against the
     specified server.
     Args:
         server: server connection as [endpoint, usr, pwd]
         query: cypher query
-        df.name = report_name
+        report_name: df.name
         column_order: optionally specify column order in df."""
     nc = neo4j_connect(*server)
     r = nc.commit_list([query])
     dc = results_2_dict_list(r)
     report = pd.DataFrame.from_records(dc)
-    report.name = report_name
     report.replace(np.nan, '', regex=True, inplace=True)
+    report.name = report_name
     if column_order:
-        return report[column_order]
+        out = report[column_order]
+        out.name = report_name
+        return out
     else:
         return report
 
 
-def gen_dataset_report(server, report_name, production_only = False):
+def gen_dataset_report(server,
+                       report_name,
+                       production_only=False):
 
     po = ''
     if production_only:
         po = " WHERE ds.production is true "
-
     return gen_report(
         server,
         query="MATCH (ds:DataSet) with ds " + po +
         "OPTIONAL MATCH (ds)-[:has_reference]->(p:pub) "
+        "WITH ds, p "   
         "OPTIONAL MATCH (ds)-[:has_license]->(l:License) "
-        "WITH ds, p OPTIONAL MATCH (ds)<-[:has_source]-(i:Individual)"
-        " RETURN ds.short_form, ds.label, ds.production, l.label as license "
-        "p.short_form as pub, count(i) as individuals order by ds.short_form",
+        "WITH ds, p, l "
+        "OPTIONAL MATCH (ds)<-[:has_source]-(i:Individual) " +
+        " RETURN ds.short_form, ds.label, ds.production, "
+        "l.label as license,  p.short_form as pub, "
+        "count(i) as individuals order by ds.short_form",
         report_name=report_name, column_order=['ds.short_form',
                                                'ds.label',
                                                'ds.production',
                                                'pub',
                                                'license',
                                                'individuals'])
+
+
+def gen_dataset_report_prod(server, report_name):
+    return gen_report(
+        server,
+        query="MATCH (ds:DataSet) WHERE ds.production = True WITH ds "
+              "OPTIONAL MATCH (ds)-[:has_reference]->(p:pub) "
+              "WITH ds, p "
+              "OPTIONAL MATCH (a:Class)-[:has_reference]->(p) "
+              "WITH ds, p, count(distinct a) as ontology_terms "
+              "OPTIONAL MATCH (:Individual)-[r:overlaps { pub: p.short_form}]->(:Expression_pattern) "
+              "WITH ds, p, ontology_terms, COUNT (distinct r) as exp_cur "         
+              "OPTIONAL MATCH (ds)-[:has_license]->(l:License) "
+              "WITH ds, p, exp_cur, ontology_terms, l "
+              "OPTIONAL MATCH (ds)<-[:has_source]-(i:Individual) "
+              "RETURN ds.short_form, ds.label,"
+              "l.label as license, p.short_form as pub, "
+              "count(distinct i) as individuals, exp_cur, ontology_terms "
+              "order by ds.short_form",
+        report_name=report_name, column_order=['ds.short_form',
+                                               'ds.label',
+                                               'license',
+                                               'pub',
+                                               'individuals',
+                                               'ontology_terms',
+                                               'exp_cur'])
+
 
 
 def diff_report(report1: pd.DataFrame, report2: pd.DataFrame):
