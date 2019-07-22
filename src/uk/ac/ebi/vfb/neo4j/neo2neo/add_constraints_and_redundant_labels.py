6e7df5588d63f321c6af2a7c294457b83b8755c9
dosumis <dosumis@gmail.com>
2019-07-22 14:13:59 +0100

    Fixes #129

--- src/uk/ac/ebi/vfb/neo4j/neo2neo/add_constraints_and_redundant_labels.py
+++ src/uk/ac/ebi/vfb/neo4j/neo2neo/add_constraints_and_redundant_labels.py
@@ -41,40 +41,39 @@ nc.commit_list(deletions)
 
 
 label_types = {
-   'Neuron': 'neuron',
-   'Sensory_neuron': 'sensory neuron',
-   'Motor_neuron' : 'motor neuron',
-   'Peptidergic_neuron' : 'peptidergic neuron', 
-   'Neuron_projection_bundle' : 'neuron projection bundle',
-   'Synaptic_neuropil': 'synaptic neuropil',
-   'Synaptic_neuropil_domain': 'synaptic neuropil domain',
-   'Synaptic_neuropil_subdomain': 'synaptic neuropil subdomain',
-   'Synaptic_neuropil_block': 'synaptic neuropil block',   
-   'Clone': 'neuroblast lineage clone',
-   'Cluster': 'cluster',
-   'Neuroblast': 'neuroblast',
-   'GMC': 'ganglion_mother_cell',
-   'Anatomy': 'anatomical entity',
-   'Cell': 'cell',
-   'Glial_cell': 'glial cell',
-   'Expression_pattern': 'intersectional expression pattern',
-   'Expression_pattern': 'expression pattern',
-   'Ganglion': 'ganglion',
-   'Cholinergic': 'cholinergic neuron',
-   'Glutamatergic': 'glutamatergic neuron',
-   'GABAergic': 'GABAergic neuron',
-   'Octopaminergic': 'octopaminergic neuron',
-   'Dopaminergic': 'dopaminergic neuron',
-   'Serotonergic': 'serotonergic neuron',
-   'Expression_pattern_fragment': 'expression pattern fragment',
-   'Neuromere': 'neuromere',
-   'Muscle': 'Muscle cell'
+   'Neuron': ['neuron'],
+   'Sensory_neuron': ['sensory neuron'],
+   'Motor_neuron' : ['motor neuron'],
+   'Peptidergic_neuron' : ['peptidergic neuron'],
+   'Neuron_projection_bundle' : ['neuron projection bundle'],
+   'Synaptic_neuropil': ['synaptic neuropil'],
+   'Synaptic_neuropil_domain': ['synaptic neuropil domain'],
+   'Synaptic_neuropil_subdomain': ['synaptic neuropil subdomain'],
+   'Synaptic_neuropil_block': ['synaptic neuropil block'],
+   'Clone': ['neuroblast lineage clone'],
+   'Cluster': ['cluster'],
+   'Neuroblast': ['neuroblast'],
+   'GMC': ['ganglion_mother_cell'],
+   'Anatomy': ['anatomical entity'],
+   'Cell': ['cell'],
+   'Glial_cell': ['glial cell'],
+   'Expression_pattern': ['expression pattern','intersectional expression pattern'],
+   'Ganglion': ['ganglion'],
+   'Cholinergic': ['cholinergic neuron'],
+   'Glutamatergic': ['glutamatergic neuron'],
+   'GABAergic': ['GABAergic neuron'],
+   'Octopaminergic': ['octopaminergic neuron'],
+   'Dopaminergic': ['dopaminergic neuron'],
+   'Serotonergic': ['serotonergic neuron'],
+   'Expression_pattern_fragment': ['expression pattern fragment'],
+   'Neuromere': ['neuromere'],
+   'Muscle': ['Muscle cell']
    }
 
 label_additions = []
 for k,v in label_types.items():
-    label_additions.append("MATCH (n)-[r:SUBCLASSOF|INSTANCEOF*]->(n2:Class) " \
-                           "WHERE n2.label = '%s' SET n:%s, n2:%s" % (v, k, k))
+    label_additions.append("MATCH (n)-[r:SUBCLASSOF|INSTANCEOF*]->(n2:Class) "
+                           "WHERE n2.label in %s SET n:%s, n2:%s" % (str(v), k, k))  # Relies on coincidence of Python/Cypher list syntax
 
 label_additions.append("MATCH (a:Individual)<-[d:Related]-(ch:Individual)-[r:Related]->(fbbi:Class) " \
                        "WHERE fbbi.label = 'computer graphic' and d.short_form = 'depicts' " \
@@ -82,7 +81,7 @@ label_additions.append("MATCH (a:Individual)<-[d:Related]-(ch:Individual)-[r:Rel
 
 label_additions.append("MATCH (n:pub) WHERE NOT n:Individual SET n:Individual")
 
-label_additions.extend(["MATCH (a:Class)SET a:Entity", "MATCH (a:Individual)SET a:Entity"]) # Entity exclude Property. Not queried
+label_additions.extend(["MATCH (a:Class)SET a:Entity", "MATCH (a:Individual)SET a:Entity"])  # Entity excludes Property. Not queried
 
 nc.commit_list(label_additions)
 
@@ -93,7 +92,7 @@ index_labels = ['Entity', 'DataSet', 'pub', 'Site', 'Expression_pattern', 'Licen
 
 index_additions = []
 
-for il in index_labels():
+for il in index_labels:
     index_additions.append("CREATE INDEX ON :%s(short_form)" % (il))
 
 nc.commit_list(index_additions)
