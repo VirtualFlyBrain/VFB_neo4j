26f6b1dbd81558a3118c7df36456b961880cbc1c
Rob Court <rcourt@ed.ac.uk>
2020-05-14 12:15:34 +0100

    adding debug output

--- src/uk/ac/ebi/vfb/neo4j/owl2neo/owl2neo_tools.py
+++ src/uk/ac/ebi/vfb/neo4j/owl2neo/owl2neo_tools.py
@@ -20,10 +20,13 @@ class OWLery2Neo:
     def owl_query_2_neo_labels(self, queries, query_by_label=True):
         label_additions = []
         for nl, q in queries.items():
+            print("Adding label '" + nl + "' using query: " + q)
             qr = self.oc.get_subclasses(q, query_by_label=query_by_label)
             if qr:
+                print("Results found...")
                 qr_chunks = chunks(qr, 500)
                 for c in qr_chunks:
+                    print("Adding label '" + nl + "' to: " + str(c))
                     label_additions.append("MATCH (e:Entity) WHERE e.iri IN %s SET e:%s" % (str(c), nl))
             else:
                 warnings.warn("No results for '%s' returned" % q)
