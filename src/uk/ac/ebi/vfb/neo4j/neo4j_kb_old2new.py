8461e397c01b5e30ca8ba9858b95fcbe46fff67d
matentzn <nicolas.matentzoglu@gmail.com>
2020-07-14 13:05:01 +0200

    Replace label by rdfs_label to make sure that the pipeline does not fail

--- src/uk/ac/ebi/vfb/neo4j/neo4j_kb_old2new.py
+++ src/uk/ac/ebi/vfb/neo4j/neo4j_kb_old2new.py
@@ -95,7 +95,7 @@ def transform_annotation_properties_on_nodes_to_array(nc):
     
     r = query(q,nc)
     for qsl in r[0]['key']:
-        if qsl not in ["label", "iri", "short_form", "qsl"]:
+        if qsl not in ["label", "iri", "short_form", "qsl","label_rdfs"]:
             print("in")
             query(q_transform % (qsl,qsl,qsl),nc)
 
@@ -106,7 +106,7 @@ def transform_annotation_properties_on_relations_to_array(nc):
     
     r = query(q,nc)
     for qsl in r[0]['key']:
-        if qsl not in ["label", "iri", "short_form", "type", "qsl"]:
+        if qsl not in ["label", "iri", "short_form", "type", "qsl","label_rdfs"]:
             query(q_transform % (qsl,qsl,qsl),nc)
         
 def rewrite_property_keys_on_nodes_to_qsl(nc,mapping):
