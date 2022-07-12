f19fcb74efd39cecd04334e3759e83fa7c1d4bc9
dosumis <dosumis@gmail.com>
2022-07-12 15:47:01 +0100

    Update kb_tests.py
    Adding working logging.

--- src/uk/ac/ebi/vfb/neo4j/neo2neo/kb_tests.py
+++ src/uk/ac/ebi/vfb/neo4j/neo2neo/kb_tests.py
@@ -1,13 +1,10 @@
-from uk.ac.ebi.vfb.neo4j.neo4j_tools import neo4j_connect,  results_2_dict_list
+from uk.ac.ebi.vfb.neo4j.neo4j_tools import neo4j_connect, results_2_dict_list
 import sys
 import warnings
 import json
 
-# KB tests
-
-# Query for dataSets (would be helpful if these were tagged as image)
-
 nc = neo4j_connect(sys.argv[1], sys.argv[2], sys.argv[3])
+
 
 def query(query):
     q = nc.commit_list([query])
@@ -18,6 +15,7 @@ def query(query):
         return False
     else:
         return dc
+
 
 def query_ind_count(query):
     q = nc.commit_list([query])
@@ -32,7 +30,8 @@ def query_ind_count(query):
     else:
         return dc[0]['ind_count']
 
-def compare(dataset, description, query1, query2, verbose = False):
+
+def compare(dataset: str, description: str, query1: str, query2: str, log: dict, verbose=False):
     r1 = query(query1)[0]
     r2 = query(query2)[0]
     if r1['ind_count'] == r2['ind_count']:
@@ -44,26 +43,24 @@ def compare(dataset, description, query1, query2, verbose = False):
     else:
         print("Testing assertion:" + description)
         print(query2)
-        print("Result: inds_in_datset: %d ; Compliant with pattern: %d" % (r1['ind_count'],  r2['ind_count']))
-        # Should probably turn this into a report
-        bad_inds = list(set(r1['ind_list']) - set(r2['ind_list']))
-#        file = open(dataset + ".report", 'w')
-#        file.write(json.dumps(bad_inds))
-#        file.close()
+        print("Result: inds_in_datset: %d ; Compliant with pattern: %d" % (r1['ind_count'], r2['ind_count']))
+        log[description + '. Failing Inds'] = list(set(r1['ind_list']) - set(r2['ind_list']))
         return False
 
 
-datasets = nc.commit_list(["MATCH (ds:DataSet) RETURN ds.label"])  # removed "WHERE ds.schema = 'image'" as not in kb2
+datasets = nc.commit_list(
+    ["MATCH (ds:DataSet) RETURN ds.short_form"])  # removed "WHERE ds.schema = 'image'" as not in kb2
 dc = results_2_dict_list(datasets)
 
 return_state = True
 
 for d in dc:
-    ds = d['ds.label']
+    log = {}
+    ds = d['ds.short_form']
     dataset_status = True
     print("\n")
-    print ("Testing: " + ds)
-    final_clauses = " WHERE ds.label = '%s' RETURN COUNT (DISTINCT i) as ind_count" \
+    print("Testing: " + ds)
+    final_clauses = " WHERE ds.short_form = '%s' RETURN COUNT (DISTINCT i) as ind_count" \
                     ", COLLECT(i.short_form) as ind_list" % ds
     base_query = "MATCH (ds:DataSet)<-[:has_source]-(i:Individual)"
     new_base_query = "MATCH (ds:DataSet)<-[:Annotation { short_form: 'source'}]-(i:Individual)"
@@ -87,28 +84,37 @@ for d in dc:
     test_stats.append(compare(dataset=ds,
                               description="All anatomical individuals in dataset have matching channel individuals.",
                               query1=query1,
-                              query2=query2))
-    test_stats.append(compare(description="All anatomical individuals in dataset have matching registered channel individuals.",
-                              dataset=ds,
-                              query1=query1,
-                              query2=query3))
-    test_stats.append(compare(description="All anatomical individuals in dataset have matching channel individuals with imaging method",
-                              dataset=ds,
-                              query1=query1,
-                              query2=query4))
-    test_stats.append(compare(description="All anatomical individuals in dataset have matching channel, typed individuals",
-                              dataset=ds,
-                              query1=query1,
-                              query2=query5))
+                              query2=query2,
+                              log=log))
+    test_stats.append(
+        compare(description="All anatomical individuals in dataset have matching registered channel individuals.",
+                dataset=ds,
+                query1=query1,
+                query2=query3,
+                log=log))
+    test_stats.append(compare(
+        description="All anatomical individuals in dataset have matching channel individuals with imaging method",
+        dataset=ds,
+        query1=query1,
+        query2=query4,
+        log=log))
+    test_stats.append(
+        compare(description="All anatomical individuals in dataset have matching channel, typed individuals",
+                dataset=ds,
+                query1=query1,
+                query2=query5,
+                log=log))
     test_stats.append(compare(description="All anatomical individuals in dataset are typed",
                               dataset=ds,
                               query1=query1,
-                              query2=query6))
+                              query2=query6,
+                              log=log))
     if False in test_stats:
         return_state = False
+        with open(ds + ".report", 'w') as report:
+            report.write(json.dumps(log))
     else:
         print("Passes!")
-
 
 if not return_state:
     sys.exit(1)
