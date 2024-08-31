1251bbb35ab5008adac00539686d63bbd5e279ca
Huseyin Kir <73784267+hkir-dev@users.noreply.github.com>
2024-08-31 21:16:52 +0100

    Update neo4j_kb_export.py

--- src/uk/ac/ebi/vfb/neo4j/neo4j_kb_export.py
+++ src/uk/ac/ebi/vfb/neo4j/neo4j_kb_export.py
@@ -55,17 +55,23 @@ def export_entities(nc, entity_count, outfile):
         page_start = page_start + page_size
 
 def export_relations(nc, outfile):
+    page_size = (entity_count // 4) + 10  # create 4 chunks
+    page_start = 0
+    page_count = 0 
+
     file_path = Path(outfile)
     file_name_without_extension = file_path.stem
     file_extension = file_path.suffix
     parent_directory = file_path.parent
 
-    out_name = f"{file_name_without_extension}_rels{file_extension}"
-    out_path = os.path.join(parent_directory, out_name)
-
-    q_generate = 'CALL ebi.spot.neo4j2owl.exportOWLEdges()'
-    o = query(q_generate,nc)[0]['o']
-    write_ontology(o, out_path)
+    while page_start < entity_count:
+        q_generate = f'CALL ebi.spot.neo4j2owl.exportOWLEdges({page_start},{page_size})'
+        o = query(q_generate,nc)[0]['o']
+        part_name = f"{file_name_without_extension}_rels_{page_count}{file_extension}"
+        part_path = os.path.join(parent_directory, part_name)
+        write_ontology(o, part_path)
+        page_count = page_count + 1
+        page_start = page_start + page_size
 
 kb=sys.argv[1]
 user=sys.argv[2]
