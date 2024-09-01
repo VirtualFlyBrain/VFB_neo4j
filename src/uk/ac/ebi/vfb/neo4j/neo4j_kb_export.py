72a641fac764552352171387cfd036af49d96acd
Rob Court <rcourt@ed.ac.uk>
2024-09-01 12:18:22 +0100

    fix for var

--- src/uk/ac/ebi/vfb/neo4j/neo4j_kb_export.py
+++ src/uk/ac/ebi/vfb/neo4j/neo4j_kb_export.py
@@ -78,7 +78,7 @@ def export_relations(nc, outfile):
     # Generate object properties in chunks
     chunk_count = 5
     for i in range(chunk_count):
-        q_generate = 'CALL ebi.spot.neo4j2owl.exportOWLEdges("objectProperty", i, chunk_count)'
+        q_generate = f'CALL ebi.spot.neo4j2owl.exportOWLEdges("objectProperty", {i}, {chunk_count})'
         o = query(q_generate,nc)[0]['o']
         out_name = f"{file_name_without_extension}_rels_3_{i}{file_extension}"
         write_ontology(o, os.path.join(parent_directory, out_name))
